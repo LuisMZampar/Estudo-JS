6bf1dd8a86b413ca07520e04abf7026287ce626a
LuisMZampar <rm550531@fiap.com.br>
2023-08-11 17:40:40 -0300

    Estudando, Aula 30 e 33

---
+++ Aula-Youtube (JS)/Aula33-QuerySelector/script33.js
@@ -0,0 +1,7 @@
+
+
+const cursoEspecial = document.getElementById("c1")
+
+const query_divTodas = document.querySelector("div")
+
+console.log(query_divTodas)
---
+++ Aula-Youtube (JS)/Aula34-addEventListener/script34.js
@@ -0,0 +1,8 @@
+// const c1 = document.getElementById("c1")
+const c1 = document.querySelectorSll(".curso")
+
+
+c1.addEventListener("click", (evt)=>{
+    const el = evt.target
+    el.add("Destaque")
+})
